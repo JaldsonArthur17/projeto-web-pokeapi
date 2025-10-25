7d8bd3b5ed55b68b7fa31a32941ec1659de21bcd
Arthur <jaldsonarthur1322@gmail.com>
2025-10-25 20:40:28 -0300

    v1.1: exclui as pastas desnecessárias

--- atividades/atv1.py
+++
@@ -1,7 +0,0 @@
-import random
-tupla = []
-for i in range(6):
-    num = random.randint(1,10)
-    tupla.append(num)
-new_tuple = tupla
-print(new_tuple)
--- atividades/atv2.py
+++
@@ -1,5 +0,0 @@
-tupla = ((1,"um"), (2,"dois"), (3,"tres"))
-
-for e in tupla:
-    if isinstance(e[0],int):
-        print(e[1])
