a9e668818bd72490ad2ce76eb3c8e0d4c7d8f656
Arthur <jaldsonarthur1322@gmail.com>
2025-10-26 16:53:05 -0300

    v1.9: criei os consts no JS pra tela inicial

--- proj-pokemon/js/tela_inicial.js
+++ proj-pokemon/js/tela_inicial.js
@@ -1,15 +1,20 @@
-const imagem = document.getElementById("imagem")
-console.log(imagem)
+document.addEventListener('DOMContentLoaded', () => {
 
-async function funcaoAssincrona() {
-    const requisicao = await fetch("https://pokeapi.co/api/v2/pokemon/1", {
-        method: "GET",
-    });
+    const pokemonListElement = document.getElementById('lista_pokemons');
 
-    const dados = requisicao.json();
-    console.log(dados);
-    return requisicao;
-}
+    const favoriteFilterButton = document.getElementById('botao_favoritar');
 
-console.log(funcaoAssincrona());
-console.log("flsd")
+    const statusMessage = document.getElementById('mensagem_status');
+
+    const prevButton = document.getElementById('botao_anterior');
+    const nextButton = document.getElementById('botao_posterior');
+
+    const detailsModal = document.getElementById('detalhes_modal');
+    const pokemonDetailsElement = document.getElementById('detalhes_pokemon');
+    const closeButton = document.getElementById('.close-button');
+
+    const pokeapi_base_url = 'https://pokeapi.co/api/v2/pokemon';
+    const pokemonLimit = 20;
+    let currentOffset = 0;
+    let isFilteringFavorites = false;
+})
