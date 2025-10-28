de7649c5abf9bb52d6dd4528dd5a5a5ae99d656e
Arthur <jaldsonarthur1322@gmail.com>
2025-10-27 23:44:32 -0300

    v2.2: melhorei o arquivo JS

--- proj-pokemon/js/tela_inicial.js
+++ proj-pokemon/js/tela_inicial.js
@@ -1,3 +1,28 @@
+//estrutura do bloco de pesquisa//
+
+const PokeNome = Document.querySelector('.Nome_Pokemon')
+const PokeNumero = Document.querySelector('.Numero_Pokemon')
+const PokeImagem = Document.querySelector('.Imagem_Pokemon')
+const PokeForms = Document.querySelector('.Form_Pokemon')
+
+
 const fetchPokemon = async (pokemon) => {
     const apiResposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    const data = await apiResposta.json();
+
+    console.log(data);
 }
+
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    PokeNome.innerHTML = data.name;
+    PokeNumero.innerHTML = data.id;
+    PokeImagem.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+
+    console.log(data);
+
+}
+
