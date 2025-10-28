1ffdbea8d2d78a1f8f05f6ec18f98ed8327a028c
Arthur <jaldsonarthur1322@gmail.com>
2025-10-28 01:24:27 -0300

    V#.0: corrigi erros de ortografia que passaram batidos (Document, click e foms)

--- proj-pokemon/js/tela_inicial.js
+++ proj-pokemon/js/tela_inicial.js
@@ -3,30 +3,27 @@
 const PokeNome = Document.querySelector('.Nome_Pokemon')
 const PokeNumero = Document.querySelector('.Numero_Pokemon')
 const PokeImagem = Document.querySelector('.Imagem_Pokemon')
+
 const PokeForms = Document.querySelector('.Form_Pokemon')
 const input = Document.querySelector('.input_pesquisa')
-
-const botaoAnterior = document.querySelector('.botao_anterior');
-const botao_posterior = document.querySelector('.botao_posterior');
+const botaoAnterior = Document.querySelector('.botao_anterior');
+const botao_posterior = Document.querySelector('.botao_posterior');
 
 let procurarPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
     const apiResposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    if (apiResposta.status == 200) {
+    if (apiResposta.status === 200) {
 
         const data = await apiResposta.json();
     return data;
 
     }
 }
-
 const renderPokemon = async (pokemon) => {
-
     PokeNome.innerHTML = 'carregando...';
     PokeNumero.innerHTML = '';
-
     const data = await fetchPokemon(pokemon);
 
     if (data) {
@@ -38,13 +35,12 @@ const renderPokemon = async (pokemon) => {
 
     } else {
         PokeImagem.style.display = 'none';
-        PokeNome.innerHTML = "Não encontrado."
+        PokeNome.innerHTML = "Não encontrado.";
         PokeNumero.innerHTML = '';
     }
-
 }
 
-form.addEventListener('submit', (Event) => {
+PokeForms.addEventListener('submit', (Event) => {
     Event.preventDefault();
     renderPokemon(input.value.toLowerCase());
 });
@@ -56,11 +52,8 @@ botaoAnterior.addEventListener('click', () => {
     }
 });
 
-botao_posterior.addEventListener('clic', () => {
+botao_posterior.addEventListener('click', () => {
     procurarPokemon += 1;
     renderPokemon(procurarPokemon);
-
 });
-
-
 renderPokemon(procurarPokemon);
